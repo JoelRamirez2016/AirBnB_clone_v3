00f36e26823ad6caa61d96d32fb96b21510f05d5
Seb Asus VS <sebas93cay@gmail.com>
2021-09-18 12:01:17 -0500

    start point 4

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""app"""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+
+HBNB_API_HOST = os.getenv('HBNB_API_HOST')
+HBNB_API_PORT = os.getenv('HBNB_API_PORT')
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exeption):
+    """Close database"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=HBNB_API_PORT or 5000,
+            host=HBNB_API_HOST or '0.0.0.0', threaded=True)
---
+++ api/v1/views/__init__.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+
+# app_views = Blueprints('app_views', )
+app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
